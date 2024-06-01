7ecf5551aeb551ffd0c06320b794d8d8adfaad17
Breqker <gia.ferrara27@gmail.com>
2024-06-01 17:26:19 +0200

    cambia id_palinsesto con ref_palinsesto

--- src/main/webapp/Schermata_monitoraggio/monitoraggio.js
+++ src/main/webapp/Schermata_monitoraggio/monitoraggio.js
@@ -5,11 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-
     console.log("ImpiantiWithStatus JSON:", impiantiWithStatusJson);
 
     impiantiWithStatusJson.forEach(function(impianto_ws) {
-
         var iconUrl = impianto_ws.isActive ? 'immagini/switch-on.png' : 'immagini/switch-off.png';
         var icon = L.icon({
             iconUrl: iconUrl,
@@ -18,10 +16,8 @@ document.addEventListener("DOMContentLoaded", function() {
             popupAnchor: [0, -16]
         });
 
-
         var marker = L.marker([impianto_ws.latitudine, impianto_ws.longitudine], { icon: icon })
             .addTo(map);
-
 
         function getAddress(lat, lon, callback) {
             var url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lon}`;
@@ -38,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 })
                 .catch(error => console.error('Errore:', error));
         }
-
 
         getAddress(impianto_ws.latitudine, impianto_ws.longitudine, function(addressData) {
             var address = addressData.display_name || 'Indirizzo non disponibile';
